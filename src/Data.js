170bd29b33a4091f4ceabdc7a6aa90045285fbab
vaibhavpatilSunorbit <vaibhav.pail@sunorbit.in>
2025-06-13 19:02:06 +0530

    fix: Update API_BASE_URL to point to the production server

--- src/Data.js
+++ src/Data.js
@@ -3,8 +3,8 @@ import { UilUniversity, UilUserPlus, UilFileAlt } from '@iconscout/react-unicons
 
 
 
-// export const API_BASE_URL = "https://laboursandbox.vjerp.com";
-export const API_BASE_URL = "http://localhost:4000";
+export const API_BASE_URL = "https://laboursandbox.vjerp.com";
+// export const API_BASE_URL = "http://localhost:4000";
 // export const API_BASE_URL = "https://vjlabour.vjerp.com";
 
 
--- src/components/ViewDetails/ViewDetails.jsx
+++ src/components/ViewDetails/ViewDetails.jsx
@@ -731,8 +731,8 @@ import Slider from '@mui/material/Slider';
 import { API_BASE_URL } from '../../Data';
 
 const trimUrl = (url) => {
-  const baseUrl = "http://localhost:4000/uploads/";
-  // const baseUrl = "https://laboursandbox.vjerp.com/uploads/";
+  // const baseUrl = "http://localhost:4000/uploads/";
+  const baseUrl = "https://laboursandbox.vjerp.com/uploads/";
   // const baseUrl = "https://vjlabour.vjerp.com/uploads/";
   return typeof url === 'string' ? url.replace(baseUrl, '') : '';
 };
