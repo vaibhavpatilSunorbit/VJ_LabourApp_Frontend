aa6d6d83f25b74b267386fffc954e78ca65c33b0
vaibhavpatilSunorbit <vaibhav.pail@sunorbit.in>
2025-06-30 15:34:21 +0530

    Remove unnecessary whitespace and clean up the ExportAttendance component

--- src/components/AttendanceReport/ImportExportAttendance/ExportAttendance.jsx
+++ src/components/AttendanceReport/ImportExportAttendance/ExportAttendance.jsx
@@ -1,6 +1,3 @@
-
-
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {
@@ -397,7 +394,6 @@ const ExportAttendance = () => {
 };
 
 export default ExportAttendance;
-
 
 
 
