8d2502b7c9ae07d61ef610d70c3bc64791628fd5
vaibhavpatilSunorbit <vaibhav.pail@sunorbit.in>
2025-07-18 11:54:09 +0530

    Add payroll icon import to Sidebar component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -143,6 +143,7 @@ import profileIcon11 from '../../images/datatransfer.png';
 // import profileIcon12 from '../../images/salaryRegister.png';
 import profileIcon13 from '../../images/wages.png';
 import profileIcon14 from '../../images/payroll.png';
+import profileIcon15 from '../../images/payroll.png';
 
 
 import VJLogo from '../../images/VJlogo-1-removebg.png';
