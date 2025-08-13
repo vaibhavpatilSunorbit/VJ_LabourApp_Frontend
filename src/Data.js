498718ef1cdd91a65c15d483f077a4ec53b1cc2b
vaibhavpatilSunorbit <vaibhav.pail@sunorbit.in>
2025-08-13 18:43:06 +0530

    Update API_BASE_URL to local development server and remove User Approval from SidebarData

--- src/Data.js
+++ src/Data.js
@@ -4,8 +4,8 @@ import { UilUniversity, UilUserPlus, UilFileAlt } from '@iconscout/react-unicons
 
 
 // export const API_BASE_URL = "https://laboursandbox.vjerp.com";
-// export const API_BASE_URL = "http://localhost:4000";
-export const API_BASE_URL = "https://vjlabour.vjerp.com";
+export const API_BASE_URL = "http://localhost:4000";
+// export const API_BASE_URL = "https://vjlabour.vjerp.com";
 
 
 // Data.js
@@ -103,13 +103,6 @@ export const SidebarData = [
     path: "Dashboard",
     roles: ["superadmin","admin", "user"],
   },
-{
-    icon: UilFileAlt,
-    heading: "User Approval",
-    path: "userApproval",
-    roles: ["user"],
-  },
-
   // {
   //   icon: UilFileAlt,
   //   heading: "People",
